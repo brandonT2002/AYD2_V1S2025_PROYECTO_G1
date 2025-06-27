45e7d912dcdc6c7340ce7128b91e71144e7d1360
Luis Yaquian <3854777400101@ingenieria.usac.edu.gt>
2025-06-26 19:16:12 -0600

    feat: pruebas fecha y digitos
    Se realizaron las pruebas agregando que la fecha cumpla con el largo y la estructura de fecha y se colocó la verificación que solo sean caracteres

--- backend/tests_imporcomgua/test_producto_controller.py
+++ backend/tests_imporcomgua/test_producto_controller.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pytest
@@ -40,7 +39,7 @@ def test_get_producto_not_found(client):
         assert data is not None
         assert data["error"] == "Producto no encontrado"
 
-
+# Nit solo 7 caracteres, aquí soo miramos el largo
 def test_nit_exacto_7_digitos():
     cliente = Cliente()
     resultado = cliente.create_cliente(
@@ -49,15 +48,32 @@ def test_nit_exacto_7_digitos():
         departamento="01",
         municipio="Guatemala",
         direccion="Zona 1",
-        nit="1234567",  # Cambiar para buscar el fallo
+        nit="1234567",  # Se peude cambair a 123456 o 12345678 para que falle
         encargado_bodega="Pedro",
         telefono="12345678",
         tipo_venta="Contado",
         observaciones=""
     )
-    assert isinstance(resultado, tuple), "El NIT no tiene exactamente 7 dígitos numéricos"
+    assert isinstance(resultado, tuple), "El NIT no tiene exactamente 7 caracteres"
 
+# Valdier que que el NIT sea numeros
+def test_nit_solo_numeros():
+    cliente = Cliente()
+    resultado = cliente.create_cliente(
+        nombre_negocio="NIT Numérico",
+        nombre_contacto="Luis",
+        departamento="02",
+        municipio="Mixco",
+        direccion="Zona 2",
+        nit="1234567",   # Cambiar a "12345A67" para que de error
+        encargado_bodega="Carlos",
+        telefono="87654321",
+        tipo_venta="Crédito",
+        observaciones=""
+    )
+    assert isinstance(resultado, tuple), "El NIT debe contener solo números"
 
+# Telefono 8 digios, aquí es el largo 
 def test_telefono_exacto_8_digitos():
     cliente = Cliente()
     resultado = cliente.create_cliente(
@@ -68,18 +84,18 @@ def test_telefono_exacto_8_digitos():
         direccion="Zona 2",
         nit="7654321",
         encargado_bodega="Luis",
-        telefono="87654321",  # Cambiar para buscar el fallo
+        telefono="87654321",  # Se puede cambiar a 123456789 para que falle
         tipo_venta="Crédito",
         observaciones=""
     )
     assert isinstance(resultado, tuple), "El teléfono no tiene exactamente 8 dígitos numéricos"
 
-
+# Prba solo pueda tener 10 dígitos con la estrucutra que se coloca 
 def test_fecha_formato_correcto():
     inventario = InventarioModel()
     resultado = inventario.insertar_duca(
         numero_duca="DUCA123",
-        fecha="2025-06-26",  # Se cambia el formato para que de el error 
+        fecha="2025-06-26",  # Cambair a  2025-6-26 paraque falle
         numero_contendor="CONT123",
         numero_duca_rectificada=None,
         fecha_duca_rectificada=None
