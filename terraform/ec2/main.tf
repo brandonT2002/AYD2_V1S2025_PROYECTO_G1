363a5cec83512c38768f5831087a4757d703cc89
brandont2002 <brandonteja02@gmail.com>
2025-06-27 17:32:15 -0600

    ci: deploy ec2

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -2,12 +2,10 @@ provider "aws" {
     region = "us-east-1"
 }
 
-# Grupo de seguridad existente (ya permite tráfico en puerto 5000)
 data "aws_security_group" "rds_existing" {
     id = "sg-019fdb7f717ebdf1a"
 }
 
-# Cargar script de inicialización (user_data) con templatefile
 locals {
     user_data_script = templatefile("${path.module}/user_data.sh.tpl", {
         github_token = var.github_token
@@ -18,9 +16,8 @@ locals {
     })
 }
 
-# Crear la instancia EC2 con el script
 resource "aws_instance" "app_server" {
-    ami                    = "ami-0a7d80731ae1b2435" # Ubuntu 22.04
+    ami                    = "ami-0a7d80731ae1b2435"
     instance_type          = "t2.micro"
     key_name               = var.ec2_key_name
     vpc_security_group_ids = [data.aws_security_group.rds_existing.id]
