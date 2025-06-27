f6e189788895958ee649d4664f54306d0cf86033
J-Ajsivinac <meso.israelajsivinac@gmail.com>
2025-06-26 20:21:42 -0600

    feat: implement protected routes for user access control; add loading state and role validation; include LoginPage and navigation items based on user role

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,30 +8,51 @@ import Vendedores from "./pages/Vendedores";
 import PaymentPage from "./pages/Pagos";
 import VentaPage from "./pages/Ventas";
 import { Toaster } from "sonner";
+import LoginPage from "./pages/Login";
+import { useAuth } from "./context/AuthContext";
+import { getNavigationItemsForUser } from "./components/layout/Navbar";
+import ProtectedRoutes from "./ProtectedRoutes";
 
 function App() {
+    const { user } = useAuth();
+    const navigationItemsbyRole = getNavigationItemsForUser(
+        user.rol_id ? user.rol_id : 0
+    );
     return (
         <Router>
             <Toaster position="top-center" richColors />
-            <Navbar logoTitle="IMPORCOMGUA" />
+            <Navbar
+                logoTitle="IMPORCOMGUA"
+                navigationItems={navigationItemsbyRole}
+            />
             <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 py-2">
                 <Routes>
-                    <Route path="/bodega" element={<IndexPage />} />
-                    <Route path="/cobranzas" element={<PaymentPage />} />
-                    <Route path="/ventas" element={<VentaPage />} />
-                    <Route path="/inventario" element={<Inventory />} />
+                    <Route path="/" element={<LoginPage />} />
                     <Route
-                        path="/mantenimiento/productos"
-                        element={<AgregarProducto />}
-                    />
+                        element={<ProtectedRoutes rolesPermitidos={[1, 3]} />}
+                    >
+                        <Route path="/bodega" element={<IndexPage />} />
+                        <Route path="/inventario" element={<Inventory />} />
+                        <Route
+                            path="/mantenimiento/productos"
+                            element={<AgregarProducto />}
+                        />
+                    </Route>
                     <Route
-                        path="/mantenimiento/clientes"
-                        element={<ClientePage />}
-                    />
-                    <Route
-                        path="/mantenimiento/vendedores"
-                        element={<Vendedores />}
-                    />
+                        element={<ProtectedRoutes rolesPermitidos={[1, 2]} />}
+                    >
+                        <Route path="/cobranzas" element={<PaymentPage />} />
+                        <Route path="/ventas" element={<VentaPage />} />
+
+                        <Route
+                            path="/mantenimiento/clientes"
+                            element={<ClientePage />}
+                        />
+                        <Route
+                            path="/mantenimiento/vendedores"
+                            element={<Vendedores />}
+                        />
+                    </Route>
                 </Routes>
             </main>
         </Router>
